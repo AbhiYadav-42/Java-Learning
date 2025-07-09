1add2d6300f97621681d24678209ff112534f581
AbhiYadav-42 <1alchemax42@gmail.com>
2025-07-09 14:55:55 +0530

    Variable

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Bro code!!!";
+        String food = "pizza";
+        String email = "fake123@gmail.com";
+        String color = "red";
+        String car = "Mushtang";
+        
+        int age = 25;
+        int year = 2024;
+        String currency = "$";
+        double price = 25999.99;
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old.");
+        System.out.println("Your choice is a " + color + " " + year + " " + car);
+        System.out.println("The price is: " + currency + price);
+        System.out.println("The food: " + food +" email is " + email);
+    }
+}
