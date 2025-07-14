43a3d7a472b394c4fe5266f35a7862e09ed70b1f
Abhi Yadav <1alchemax42@gmail.com>
2025-07-15 03:06:59 +0530

    Got to know Arithmetic operators

---
+++ airthmetic_operator.java
@@ -0,0 +1,24 @@
+public class airthmetic_operator {
+  public static void main(String[] args) {
+    
+
+    //    Arithmetic Operators
+
+    int x =10;
+    int y = 2 ; 
+    int z;
+    
+    z = x / y;
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x % y;
+
+    System.out.println((z));
+
+    //  
+
+
+
+  }
+}
