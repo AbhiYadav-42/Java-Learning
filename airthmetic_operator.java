486c83af04f03fe6c1d1729f775d3aaf922c1cd9
Abhi Yadav <1alchemax42@gmail.com>
2025-07-15 03:37:01 +0530

    Completed Airthmetic operators--> Learned bunch of things

--- airthmetic_operator.java
+++ airthmetic_operator.java
@@ -16,7 +16,30 @@ public class airthmetic_operator {
 
     System.out.println((z));
 
-    //  
+    //  Augmented Assigment operators
+
+    // x +=y;
+    // x -=y;
+    // x *=y;
+    // x /=y;
+    // x %=y;
+    
+    System.out.println(x);
+    
+    // increment and decrement operators
+
+    int c=1;
+    c++;    // increment
+    c--;    // decrement
+
+    System.out.println(c);
+
+
+    //  Order of operations {P-EM-D-A-S}
+
+
+    double result  = 3 + 4 *( 7 - 5 ) / 2.0;
+    System.out.println(result);
 
 
 
