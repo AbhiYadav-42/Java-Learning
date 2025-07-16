252812533c92a44f3078a477de1e1a7de32ceab7
Abhi Yadav <1alchemax42@gmail.com>
2025-07-16 14:01:22 +0530

    Just learned a random fucntion

---
+++ _Random_ly.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class _Random_ly {
+  public static void main(String[] args) {
+    Random ran = new Random();
+
+//  GERNATE RANDOM NUMBER 
+    int number;
+    number = ran.nextInt(1,21);
+
+    System.out.println("Your number: "+number);
+
+//  TOSS a COIN
+    boolean isHEADs;
+    isHEADs =  ran.nextBoolean();
+
+
+    if(isHEADs){
+      System.out.println("HEADS");
+    }
+    else{
+      System.out.println("TAILS");
+    }
+  }
+}
+
