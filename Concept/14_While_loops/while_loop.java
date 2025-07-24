fdf559f2fd2bac2ee0f709cc242efdb5dade4afc
Abhi Yadav <1alchemax42@gmail.com>
2025-07-24 21:47:54 +0530

    learning while loops

---
+++ Concept/14_While_loops/while_loop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class while_loop {
+  public static void main(String[] args) {
+    
+  // While loop = repeat some code forever
+  //        while some condition remains
+
+  Scanner scan = new Scanner (System.in);
+  String
+    
+  }
+}
