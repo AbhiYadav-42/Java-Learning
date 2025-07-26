1dcec144700f37ebf6a0c285526cabbac5d2ad05
Abhi Yadav <1alchemax42@gmail.com>
2025-07-26 16:38:05 +0530

    learning for loops

---
+++ Concept/15_For Loops/for_loops.java
@@ -0,0 +1,10 @@
+public class for_loops {
+  public static void main(String[] args) {
+    // FOR loops -> execute some code a CERTAIN amount of times.
+
+    for(int i= 0 ; i < 10; i++){
+      
+    }
+
+  }  
+}
