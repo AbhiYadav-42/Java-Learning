da73ab50e792e595123430ff9cf2a292c5c7580a
Abhi Yadav <1alchemax42@gmail.com>
2025-07-26 12:59:40 +0530

    completed while loop--do_while

--- Concept/14_While_loops/while_loop.java
+++ Concept/14_While_loops/while_loop.java
@@ -6,7 +6,45 @@ public class while_loop {
   //        while some condition remains
 
   Scanner scan = new Scanner (System.in);
-  String
+  // String name ="";
+
+  // while(name.isEmpty()){
+  //   System.out.println("Enter your name: ");
+  //   name = scan.nextLine();
+  // }
+
+  // System.out.println("Hello" + name);
+
+  //  scan.close();
+
+
+
+  // DO Whilee
+
+  // int age  = 0;
+
+  // do{
+  //   System.out.println("your age can't be negative ");
+  //   System.out.print("Enter your age: ");
+  //   age = scan.nextInt();
+  // } while(age<0);
+
+  // System.out.println("you are " + age+ " year old" );
+
+  // scan.close();
     
+
+  // do while-2
+
+  int num = 0;
+
+  do{
+    System.out.println("Enter a number between 1 to 10: ");
+    num=scan.nextInt();
+  }while(num >1 || num <10);
+
+  System.out.println("You picked : "+ num);
+scan.close();         
+
   }
-}
+  }
