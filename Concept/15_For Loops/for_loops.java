ac9c1a23f326d724a381b72af9a36bea9cee9ab1
Abhi Yadav <1alchemax42@gmail.com>
2025-07-26 17:09:30 +0530

    Completed the FOR loops

--- Concept/15_For Loops/for_loops.java
+++ Concept/15_For Loops/for_loops.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class for_loops {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
     // FOR loops -> execute some code a CERTAIN amount of times.
+    // initialization; condition; increment/decrement
+    System.out.println("Enter hoew many times you want to loops: ");
+    int max= scan.nextInt();
 
-    for(int i= 0 ; i < 10; i++){
-      
+    for(int i= 0i>=max; i++){
+      System.out.println(i);
+
     }
-
+scan.close();
   }  
 }
