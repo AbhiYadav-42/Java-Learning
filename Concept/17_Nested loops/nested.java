b72ab2bcebd5936b6c8712ac76d9f1473ca28dbd
Abhi Yadav <1alchemax42@gmail.com>
2025-07-27 13:30:49 +0530

    Nested loops

---
+++ Concept/17_Nested loops/nested.java
@@ -0,0 +1,33 @@
+public class nested {
+  public static void main(String[] args) {
+    // nested loop =  A loop inside another loop 
+    //                used often with matrices or DSA  
+  
+
+/*    
+  for(int i  =1 ; i<= 9; i++ ){
+    System.out.print(i + " ");
+  }
+  System.out.println();
+    for(int i  =1 ; i<= 9; i++ ){             // In coding we follow a principle called
+    System.out.print(i + " ");                  //           D R Y
+  }                                        //         Don't Reapeat Yourself
+  System.out.println();
+    for(int i  =1 ; i<= 9; i++ ){
+    System.out.print(i + " ");
+  }
+   */
+
+
+   // That's why we use the Nested LOOP
+
+    for(int i=1; i <= 3; i++){
+      for(int j=1; j<=9; j++){
+        System.out.print(j + " " );
+      }
+      System.out.println();
+    }
+
+  
+  }
+}
