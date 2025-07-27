bac5018e5e1e54a47dbee61872ee5db8a396beb7
Abhi Yadav <1alchemax42@gmail.com>
2025-07-28 00:29:05 +0530

    row and column

---
+++ Concept/15_For Loops/q.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class q{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner (System.in);
+    // Sum of numbers:
+    int sum=0;
+   int num;
+   System.out.print("Enter the #: ");
+   num = scan.nextInt();                                                       
+
+   for(int i = 0 ; i <= num; i++){
+    sum = sum + i;
+
+    if () {
+      
+    }
+    System.out.print(sum + " ");
+   }
+  
+   scan.close();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
