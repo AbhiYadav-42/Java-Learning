7064604ffc1689ba5d7115aeb7f2c212888118ef
Abhi Yadav <1alchemax42@gmail.com>
2025-07-27 12:29:41 +0530

    Completed FOR  loops

--- Concept/15_For Loops/for_loops.java
+++ Concept/15_For Loops/for_loops.java
@@ -1,17 +1,38 @@
 import java.util.Scanner;
 
 public class for_loops {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Scanner scan = new Scanner(System.in);
     // FOR loops -> execute some code a CERTAIN amount of times.
     // initialization; condition; increment/decrement
-    System.out.println("Enter hoew many times you want to loops: ");
-    int max= scan.nextInt();
+  
+  
+  
+    // System.out.println("Enter hoew many times you want to loops: ");
+    // int max= scan.nextInt();
 
-    for(int i= 0i>=max; i++){
+    // for(int i= 0;i>=max; i++){
+    //   System.out.println(i);
+
+    // }
+
+    
+    System.out.print("Enter the occasion: ");
+    String occ = scan.nextLine();
+
+    System.out.print("How many seconds to countdown from? ");
+    int start = scan.nextInt();
+
+    for(int i = start; i >= 0; i--){
       System.out.println(i);
-
+      Thread.sleep(1000);
     }
+    System.out.println(occ);
+
+
+
+    
+
 scan.close();
   }  
 }
