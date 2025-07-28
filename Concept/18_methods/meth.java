629afc31a451b70157aa7f5b1384ce75bdcf5b4f
Abhi Yadav <1alchemax42@gmail.com>
2025-07-28 12:31:28 +0530

    Methods

---
+++ Concept/18_methods/meth.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class meth {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    // Methods = a block of reusable code that is executed when called ()
+
+  double result = square(4);
+    System.out.println("square: "+ result);
+  
+    System.out.println("CUBE: "+ cube(4));
+  
+    String fullName = getFullName("Spongebob", "Squarepants");
+    System.out.println(fullName);
+
+    int age  =21;
+
+    if(ageCheck(age)){
+      System.out.println("YOU may sign up!");
+    }
+    else{
+      System.out.println("You must be 18+ to sign up !");
+    }
+  }
+  // Sqaure a number 
+
+  static double square(double number){
+    return number * number ; 
+  }
+
+ static double cube( double number){
+    return number * number * number;  }
+
+static String getFullName(String first, String last){
+  return first + " " + last;}
+
+static boolean ageCheck(int age){
+  if(age>= 18){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+}
