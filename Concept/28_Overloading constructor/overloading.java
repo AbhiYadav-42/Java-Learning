68e8d12e271bbfdb8e9be78d510f29e1255dcea3
Abhi Yadav <1alchemax42@gmail.com>
2025-08-05 23:16:46 +0530

    Overloading

---
+++ Concept/28_Overloading constructor/overloading.java
@@ -0,0 +1,8 @@
+public class overloading {
+      public static void main(String[] args) {
+        
+        // Overloaded constructors  = Allow a class to hve multiple construcotrs
+        //                            with different paramter lists,
+        //                            Enable objects to be initialized in various arrys 
+      }
+}
