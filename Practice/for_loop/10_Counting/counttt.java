c89f00693d48b50c4aa22f06b5a8acb40f59712b
Abhi Yadav <1alchemax42@gmail.com>
2025-08-06 23:41:20 +0530

    Pattern # 2

--- Practice/for_loop/10_Counting/counttt.java
+++ Practice/for_loop/10_Counting/counttt.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
-
 public class counttt {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner (System.in);
     // count the number = 66789
       //         count  = 5
+
+      // 66789/10
+      // unknown variable ++
+
+      // here unknown variable is count = 0;
 int count =0;
 
       System.out.print("Enter the number: ");
