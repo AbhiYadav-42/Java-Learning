f05e0b755550dd0de78a88eea8e926b7bd16d8c4
Abhi Yadav <1alchemax42@gmail.com>
2025-08-07 00:27:15 +0530

    Pattern #3

--- Practice/for_loop/11_ Pattern #2/Pattern.java
+++ Practice/for_loop/11_ Pattern #2/Pattern.java
@@ -7,9 +7,9 @@ public class Pattern {
 //          12345
 
 
-for(int i =1 ; i<=5; i++){
+for(int i =5 ; i>=1; i--){
   for(int j =1; j<=i; j++){
-    System.out.print(j);
+    System.out.print("*");
   }
   System.out.println();
 }
