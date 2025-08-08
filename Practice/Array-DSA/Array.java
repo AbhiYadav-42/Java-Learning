1d49fabd01d6d71f25d0b1897a7fcbb693928c2f
Abhi Yadav <1alchemax42@gmail.com>
2025-08-08 16:04:01 +0530

    Array workingg

--- Practice/Array-DSA/Array.java
+++ Practice/Array-DSA/Array.java
@@ -27,7 +27,33 @@ public class Array {
   // int[]--->> it is where the array is defined that what datatype is going to handle 
     //  here, it is int, means only integers
 
-
+    
+//      [ works at Compile time]
     int[] ros  // declaratioon of array.  here ros is getting defined in the stack
     =  new int[5];    // Actually here object is beign created in the memory or can say allocation of memory (inside the heap)
-}
+//                 /|\                  
+//                  |
+//            Dynamic memory allocation 
+
+//   arr =   |3 | 8 | 19 | 54 | 45 | 67| 
+
+
+// Arrays are the conitouns in nature but 
+//      in
+//  Java Arrays is not continous in mature -> it depends on the JVM
+//            1. Arrays objects are in heap 
+//            2.  Heap objects are not continous in Java
+//            3.  Dynamic memory allocation
+//  hence, arrays objects in java may not be continous data.  
+
+
+
+
+
+//            0   1   2     3    4    5
+//   arr =   |3 | 8 | 19 | 54 | 45 | 67|
+//          print(arr[0]) = 99;    // Assign   
+//          print(arr[5]);         //OUTPUT: 67 
+
+
+}                  
