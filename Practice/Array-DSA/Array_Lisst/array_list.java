aac6756e28c437ef6c20650672b3c868ff60dc5d
Abhi Yadav <1alchemax42@gmail.com>
2025-08-13 21:21:08 +0530

    learning Array_List

--- Practice/Array-DSA/Array_Lisst/array_list.java
+++ Practice/Array-DSA/Array_Lisst/array_list.java
@@ -1,7 +1,38 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class array_list {
   public static void main(String[] args) {
+
+    Scanner in = new Scanner (System.in);
     // Syntax 
-    array<Integer> list = new array<>();
+    ArrayList<Integer> list = new ArrayList<>(10);
+
+
+// we can add as much as we want it
+// though, there is an initial capacity of 10
+    list.add(999);
+    list.add(9949);
+    list.add(9569);
+    list.add(56);
+
+// We can update too     
+System.out.println(list.contains(83443343));  // true or false
+
+System.out.println(list);
+list.set(0,99);
+
+list.remove(2);
+
+
+//  input
+for(int i = 0; i < 5; i++) {
+  list.add(in.nextInt());
+  
+}
+for(int i = 0; i<5; i ++){
+  System.out.println(list.get(i));
+} 
+
+System.out.println(list);
   }
 }
