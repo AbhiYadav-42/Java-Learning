74c2a34a569d63c84a5b3ea60142c9c36675fb9e
Abhi Yadav <1alchemax42@gmail.com>
2025-08-15 17:11:37 +0530

    Added reverse fucnitons

--- Practice/Array-DSA/Q1/Swap.java
+++ Practice/Array-DSA/Q1/Swap.java
@@ -25,9 +25,19 @@ public class Swap {
  for(int i =0; i <  arr.length ; i++) {
   System.out.print(arr[i]+ " ");
 }
-    
+
+System.out.println("Here is the revrse of the swap. ");
+
+    reverse(arr);
 
   }
+
+static void reverse(int[] arr){
+for(int i = arr.length - 1; i >= 0; i--){
+    System.out.print(arr[i] + " ");
+}
+
+}
 
   static void swap(int[] arr, int index1, int index2 ){
     int temp = arr[index1];
