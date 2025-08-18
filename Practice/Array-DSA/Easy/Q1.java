3e60fce399e81330f6c0fe47418d27d9f4dac400
Abhi Yadav <1alchemax42@gmail.com>
2025-08-18 18:46:57 +0530

    Print arrays to store 5 numbers

---
+++ Practice/Array-DSA/Easy/Q1.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q1 {
+  public static void main(String[] args) {
+    // Print the array -> WAP to store 5 numbers in an array and print them.
+    Scanner in= new Scanner(System.in);
+    int [] arr = new int[5];
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
+    System.out.println(Arrays.toString(arr));
+     
+
+  }
+  
+}
