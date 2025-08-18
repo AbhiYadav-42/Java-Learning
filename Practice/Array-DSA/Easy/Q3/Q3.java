e5afc10d387aebc73a470010a8c80edb98f4d500
Abhi Yadav <1alchemax42@gmail.com>
2025-08-18 20:41:54 +0530

    Q-3

---
+++ Practice/Array-DSA/Easy/Q3/Q3.java
@@ -0,0 +1,37 @@
+
+//     Find the largest element --->> 
+// Given an array, find the largest element.
+import java.util.Scanner;
+
+
+class  MaxVal{
+public static  int maxVal (int [] arr ){
+   int max = arr[0];
+ for(int i=0; i <arr.length; i ++){
+  if (arr[i] > max){
+      max = arr[i] ;
+  }
+}
+return max;
+}
+}
+ 
+
+public class Q3 {
+  public static void main(String[] args) {
+Scanner in = new Scanner (System.in);
+
+System.out.print("Size: ");
+    int n=in.nextInt();
+
+System.out.println("Array: ");
+ int [] arr = new int[n];
+ for(int i =0 ; i<arr.length; i++){
+  arr[i] = in.nextInt();
+ }
+ 
+ int max = MaxVal.maxVal(arr);
+ System.out.println("maximum: " + max);
+
+  }
+}
