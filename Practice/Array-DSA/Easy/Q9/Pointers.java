9396996c1121ad69f06114ad003db1986a183bab
Abhi Yadav <1alchemax42@gmail.com>
2025-08-21 11:18:19 +0530

    Pointer-example

---
+++ Practice/Array-DSA/Easy/Q9/Pointers.java
@@ -0,0 +1,22 @@
+public class Pointers {
+  public static void main(String[] args) {
+   int[] num = {10, 20, 30,40,50};
+
+  //left pointer
+    int left = 0;
+
+  //Right pointer
+    int right = num.length -1 ;
+
+
+    while(left <= right) {
+      System.out.print("left pointer at index" + left + " : " + num[left]);
+      System.out.println("Right pointer at index" + right + ": " + num[right] );
+      left++;
+      right++; 
+    } 
+
+
+
+  }  
+}
