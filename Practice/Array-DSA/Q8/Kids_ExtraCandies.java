f16881e187543dd912a4f6804d1ed2b59d85d346
Abhi Yadav <1alchemax42@gmail.com>
2025-08-22 23:30:10 +0530

    KIDS extraCandies

--- Practice/Array-DSA/Q8/Kids_ExtraCandies.java
+++ Practice/Array-DSA/Q8/Kids_ExtraCandies.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
+class Kids_ExtraCandies {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
       int maxVal = 0;
     for(int i =0; i<candies.length; i++){
@@ -20,5 +20,13 @@ List<Boolean> isKidsWithCandies = new ArrayList<>();
         }
     }    
     return isKidsWithCandies;
+    }
+
+    public static void main(String[] args) {
+      Kids_ExtraCandies solution = new Kids_ExtraCandies();
+      int[] candies = {2, 3, 5, 1, 3};
+      int extraCandies = 3;
+      List<Boolean> result = solution.kidsWithCandies(candies, extraCandies);
+      System.out.println(result);
     }
 }
