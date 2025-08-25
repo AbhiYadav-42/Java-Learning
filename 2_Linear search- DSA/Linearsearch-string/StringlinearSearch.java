ccc49e6b7d71db6e8c5dc3b23a2f9bbbd51b4e5f
Abhi Yadav <1alchemax42@gmail.com>
2025-08-25 23:35:59 +0530

    Linear search in String

---
+++ 2_Linear search- DSA/Linearsearch-string/StringlinearSearch.java
@@ -0,0 +1,22 @@
+
+public class StringlinearSearch {
+  public static void main(String[] args){
+    char target = 'u';
+    String name = "Kunal";
+    System.out.println(search(name, target));
+  }
+
+static boolean search(String str, char target){
+    if(str.length() == 0){  
+      return false;
+    }
+
+
+  for(int i = 0; i<str.length(); i++){
+    if(target == str.charAt(i)){
+      return true;
+    }
+  }
+  return false;
+  }
+}
