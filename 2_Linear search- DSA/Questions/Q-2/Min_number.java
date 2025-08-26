d61a54de926fc435a8a892b95836f0ef357b160d
Abhi Yadav <1alchemax42@gmail.com>
2025-08-26 11:48:15 +0530

    Minimum search in array

--- 2_Linear search- DSA/Questions/Q-2/Min_number.java
+++ 2_Linear search- DSA/Questions/Q-2/Min_number.java
@@ -1,11 +1,21 @@
 
-
-public class Max_number {
+// Q. Find the minimum element in the array. 
+public class Min_number {
 public static void main(String[] args) {
   int [] arr = {14, 12,-4};
   System.out.println(min(arr));
 
 } 
+// asume arr.length ! = 0
+// return the minimum value in the array
+static int min(int[] arr){
+  int ans = arr[0];
+  for(int i =1; i<arr.length; i++){
+    if(arr[i]<ans){
+      ans = arr[i];
+    }
+  }
+  return ans;
 
-static int min (int )
+}
 }
