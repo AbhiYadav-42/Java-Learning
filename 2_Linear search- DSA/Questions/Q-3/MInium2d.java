c8a28b0a58971f6a21392617d4abe6e34e13dd06
Abhi Yadav <1alchemax42@gmail.com>
2025-08-26 21:11:29 +0530

    mimmum and maximum in 2D Array- LinearSearch

--- 2_Linear search- DSA/Questions/Q-3/MInium2d.java
+++ 2_Linear search- DSA/Questions/Q-3/MInium2d.java
@@ -26,6 +26,16 @@ public class MInium2d {
     }
     return min;
   }
-
+  static int search2(int [][] arr){
+    int max = arr[0][0];
+    for(int row = 0; row<arr.length; row++){
+      for(int col = 0; col<arr[row].length; col++){
+        if(arr[row][col]> max){
+          max = arr [row][col];
+        }
+      }
+    }
+    return max;
+  }
 
 }
