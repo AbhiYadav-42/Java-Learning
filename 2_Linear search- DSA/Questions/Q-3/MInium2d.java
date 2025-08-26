432a630ba03d912382f3dbb32551f5fe10235d1c
Abhi Yadav <1alchemax42@gmail.com>
2025-08-26 21:10:35 +0530

    LinearSearch mimium element in 2D Array

---
+++ 2_Linear search- DSA/Questions/Q-3/MInium2d.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class MInium2d {
+  public static void main(String[] args) {
+    int [][] arr = {
+                {12,34,56},
+                {1,4,7,89},
+                {2,4,66},
+    };
+    int minimum = search(arr);
+    int maximum = search2(arr);
+
+    System.out.println(minimum);
+    System.out.println(maximum);
+
+  }
+
+  static int search(int [][] arr){
+    int min = arr[0][0];
+    for(int row = 0; row<arr.length; row++){
+      for(int col = 0; col<arr[row].length; col++){
+        if(arr[row][col]< min){
+          min = arr [row][col];
+        }
+      }
+    }
+    return min;
+  }
+
+
+}
