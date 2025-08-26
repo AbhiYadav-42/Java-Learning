b964c2edf61aa9876f53a466a257c4622f72a180
Abhi Yadav <1alchemax42@gmail.com>
2025-08-26 10:46:11 +0530

    Linear search in Range

--- 2_Linear search- DSA/Questions/Q-1/SearchInrange.java
+++ 2_Linear search- DSA/Questions/Q-1/SearchInrange.java
@@ -1,13 +1,16 @@
-
+// Search for 3 in the range of index [1,4]
 
 public class SearchInrange {
- static int linearSearch(int[] arr, int target){
+ static int linearSearch(int[] arr, int target , int star , int end){
     if(arr.length == 0){
       return -1;
     }
 
+
+
+
 //  Run for loop
-    for(int i = 0 ; i< arr.length; i++){
+    for(int i = star ; i <= end ; i++){
       // Check for element at every index if it is  =  target
       int element =  arr[i];
       if(element ==  target){
@@ -15,14 +18,19 @@ public class SearchInrange {
       }
     }
 
-
-
-
-
-
 // this line will execute if  none of the statements above have executed 
 // hence the target not found!!
     return Integer.MAX_VALUE;
   }
 
+
+
+  public static void main(String[] args) {
+    int []arr = {1 , 32, 53,64,5,67,6,7,88,8,99,};
+    int target =32;
+    System.out.println(linearSearch(arr, target, 1, 4));
+  }
 }
+
+
+
