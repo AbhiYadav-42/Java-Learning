40d755f663c8646a44364265d638576797dbf012
Abhi Yadav <1alchemax42@gmail.com>
2025-09-01 23:04:51 +0530

    Binary search intro

---
+++ 3_Binary search -DSA/Intro_BinarySearch.java
@@ -0,0 +1,26 @@
+
+
+public class Intro_BinarySearch {
+
+/*              📌 Binary Search – Definition
+Binary Search is an efficient searching algorithm used on sorted arrays. 
+It works by repeatedly dividing the search interval in half:
+--> find the middle element
+--> If target is larger   target> mid → search in the right half. 
+--> else → search in the left half.
+--> If they are equal  target == mid → element found!!
+*/
+
+
+// S = start pointer*     e = End pointer* 
+//  m = mid element
+
+//         S                m                         e
+// arr = [ 2 | 4 | 6 | 9 | 11 | 12 | 14 | 20 | 36 | 48 ]
+//         0   1   2   3   4    5    6    7   8     9
+
+
+// target = 36
+
+
+}
