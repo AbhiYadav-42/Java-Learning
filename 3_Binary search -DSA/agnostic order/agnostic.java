b41a237fe2a15de427e9952ff29054568ae6201a
Abhi Yadav <1alchemax42@gmail.com>
2025-09-04 23:51:45 +0530

    Agnostic order

--- 3_Binary search -DSA/agnostic order/agnostic.java
+++ 3_Binary search -DSA/agnostic order/agnostic.java
@@ -3,7 +3,7 @@
 public class agnostic {
   public static void main(String[] args) {
   int[] arr = {-18,-12,-4,0,3,15,45,89 } ;
- int  target = 45;
+ int  target = -18;
   int ans= binarySearch(arr, target);
 
   System.out.println(ans);
@@ -14,37 +14,36 @@ public class agnostic {
     int start = 0; 
     int end = arr.length - 1;
 
-    while(start <= end){
+// find whether the array is sorte in ascending or descending 
+    boolean isAsc = arr[start] < arr[end];
+
+  while(start <= end){
 // find the middle element
 // int mid = (start +  end) / 2;    // might be possible that (start +  end) exceeds the range of int in java
 
-      int mid = start + (end - start) /2 ; 
+    int mid = start + (end - start) /2 ; 
 
-// find whether the array is sorte in ascending or descending 
-      boolean isAsc = arr[start] < arr[end];
+   if(arr[mid] == target){
+    return mid;
+   } 
+
     // Ascennding order...
     if(isAsc){
       if(target < arr[mid]){
         end = mid -1;
-      } 
-      else{
+      }  else{
         start = mid +1;
       }
     }else{
       if(target > arr[mid]){
         end =mid-1;
-      }
-      else{
+      }else{
         start = mid +1;
       }
     }
     }
     return -1;
   }
-
-
-
-
 }
 
 
