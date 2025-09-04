01d10f583c18440eef5d4f4b9a8b91c9ed26c99c
Abhi Yadav <1alchemax42@gmail.com>
2025-09-04 23:20:07 +0530

    Binsry search intro

--- 3_Binary search -DSA/Intro_BinarySearch.java
+++ 3_Binary search -DSA/Intro_BinarySearch.java
@@ -92,7 +92,42 @@ It works by repeatedly dividing the search interval in half:
 
 
 
+// bettter way to find mid 
+// m =  s + (e -s)/2 => 2s + e-s /2 => (s +e) /2
 
+
+
+public static void main(String[] args) {
+  int[] arr = {-18,-12,-4,0,3,15,45,89 } ;
+ int  target = 45;
+  int ans= binarySearch(arr, target);
+
+  System.out.println(ans);
+}
+
+  // return the index
+ static int binarySearch(int [] arr, int target) {
+    int start = 0; 
+    int end = arr.length - 1;
+
+    while(start <= end){
+      // find the middle element
+      // int mid = (start +  end) / 2;    // might be possible that (start +  end) exceeds the range of int in java
+
+      int mid = start + (end - start) /2 ; 
+
+      if(target < arr[mid]){
+        end = mid -1;
+      } else if(target > arr[mid]){
+        start = mid +1;
+      } else{
+        // ans found
+
+        return mid; 
+      }
+    }
+    return -1;
+  }
 
 
 
